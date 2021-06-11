f76843c3efa2f93f85a005a04c0ea38c60e2463f
DESKTOP-7GV9PQI\boban <bobanderic2000@gmail.com>
2021-06-11 16:55:33 +0800

    git init

---
+++ src/main/java/com/shonpee/shonpee/ShonpeeApplication.java
@@ -0,0 +1,13 @@
+package com.shonpee.shonpee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShonpeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShonpeeApplication.class, args);
+	}
+
+}
