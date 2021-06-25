7438ba17f72b6ad8dfa120898fda69f709c65a34
bobanderic2000 <bobanderic2000@gmail.com>
2021-06-25 11:54:57 +0800

    改名package ServiceRespository為servicerepository；修改'賣家銷售'的html、css

--- src/main/java/com/shonpee/shonpee/controller/LoginController.java
+++ src/main/java/com/shonpee/shonpee/controller/LoginController.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.shonpee.shonpee.ServiceRepository.MemberServiceRepository;
 import com.shonpee.shonpee.domain.MemberBean;
+import com.shonpee.shonpee.servicerepository.MemberServiceRepository;
 
 @Controller
 public class LoginController {
---
+++ src/main/java/com/shonpee/shonpee/servicerepository/OrderServiceRepository.java
@@ -0,0 +1,5 @@
+package com.shonpee.shonpee.servicerepository;
+
+public class OrderServiceRepository {
+
+}
