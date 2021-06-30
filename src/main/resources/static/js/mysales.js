e5c1b1b837195bb1b7e3efeb73b6a25c2b1d60b9
bobanderic2000 <bobanderic2000@gmail.com>
2021-06-30 19:32:39 +0800

    shonpee_order資料表的status欄位型別從varchar修改回int

--- src/main/resources/static/js/mysales.js
+++ src/main/resources/static/js/mysales.js
@@ -10,7 +10,9 @@ function setViewFromStatus() {
     console.log("setViewFromStatus()");
 
     $('.order-status').each(function(index, statusItem) {
-        // 依狀態決定該訂單的操作按鈕是否綁定事件、是否可用
+        // 先將狀態欄位的數字，代換成有邏輯意義的中文文字
+        replaceStatusNumToWord(statusItem);
+        // 後依狀態文字，決定操作按鈕是否綁定事件、是否可用
         refreshOperatorBtn(statusItem);
     });
 }
@@ -33,15 +35,13 @@ function refreshOperatorBtn(statusItem) {
             // 將"出貨"、"取消"按鈕失效
             // $shipoutBtn.off('click', changeStatus);
             // $cancelBtn.off('click', changeStatus);
-            // $shipoutBtn.prop('disabled', true);
-            // $cancelBtn.prop('disabled', true);
-            // $shipoutBtn.fadeTo("fast", 0.6).css('cursor', 'not-allowed');
-            // $cancelBtn.fadeTo("fast", 0.6).css('cursor', 'not-allowed');
+
             $(statusItem).next('.operatebtn').find('button').prop('disabled', true).fadeTo("fast", 0.6).css('cursor', 'not-allowed');
             break;
         case "取消":
             // 將"出貨"、"取消"按鈕失效
             // $shipoutBtn.off('click', changeStatus);
+            // $cancelBtn.off('click', changeStatus);
             $(statusItem).next('.operatebtn').find('button').prop('disabled', true).fadeTo("fast", 0.6).css('cursor', 'not-allowed');
             break;
     }
@@ -52,15 +52,47 @@ function changeStatus(e) {
     console.log("changeStatus()");
 
     var changingOrderId = $(this).siblings('.order-id').val();
-    var statusBeforeUpdate = $(this).closest(".operatebtn").siblings('.order-status').text();
+    var statusNumBeforeChanging = changeStatusWordToNum($(this).closest(".operatebtn").siblings('.order-status').text());
     switch ($(this).text()) {
         case "出貨":
-            ajaxChangeStatusShipped(changingOrderId, statusBeforeUpdate);
+            ajaxChangeStatusShipped(changingOrderId, statusNumBeforeChanging);
             break;
         case "取消":
             ajaxChangeStatusCanceled(changingOrderId);
             break;
     }
+}
+
+// 出貨(修改狀態)：找到指定的訂單，將訂單ID、狀態代號，用ajax的PUT方法向後端傳送
+function ajaxChangeStatusShipped(updatingOrderId, statusNumBeforeChanging) {
+    console.log("ajaxChangeStatusShipped()");
+
+    jsonData = { orderId: updatingOrderId, status: statusNumBeforeChanging }
+
+    $.ajax({
+        type: "PUT",
+        url: `/order/${updatingOrderId}`,
+        data: JSON.stringify(jsonData),
+        contentType: "application/json"
+    }).done(function(updatedOrderDataMap) {
+        // 將畫面上指定訂單的狀態欄位內容更新
+        console.log("AJAX -> DONE");
+        // console.log("---------------------------");
+        // console.log("updatedOrderDataMap 如下:");
+        // console.log(updatedOrderDataMap);
+        // console.log("updatedOrderDataMap.orderId 如下:");
+        // console.log(updatedOrderDataMap.orderId);
+        // console.log("updatedOrderDataMap.status 如下:");
+        // console.log(updatedOrderDataMap.status);
+        // console.log("type of updatedOrderDataMap.status ==> " + typeof(updatedOrderDataMap.status));
+        // console.log("---------------------------");
+
+        refreshClickedOrderItem(updatedOrderDataMap.orderId, updatedOrderDataMap.status);
+    }).fail(function() {
+        ////////// 可以在畫面塞入錯誤訊息提示，可以請使用者重新整理再操作，或是其他補救措施
+        // TODO
+        console.log("AJAX -> FAIL");
+    });
 }
 
 // 取消：找到指定的訂單，將訂單ID、狀態，用ajax的DELETE方法向後端傳送
@@ -93,46 +125,80 @@ function ajaxChangeStatusCanceled(cancelingOrderId) {
     });
 }
 
-// 出貨(修改狀態)：找到指定的訂單，將訂單ID、狀態代號，用ajax的PUT方法向後端傳送
-function ajaxChangeStatusShipped(updatingOrderId, statusBeforeUpdate) {
-    console.log("ajaxChangeStatusShipped()");
-
-    jsonData = { orderId: updatingOrderId, status: statusBeforeUpdate }
-
-    $.ajax({
-        type: "PUT",
-        url: `/order/${updatingOrderId}`,
-        data: JSON.stringify(jsonData),
-        contentType: "application/json"
-    }).done(function(updatedOrderDataMap) {
-        // 將畫面上指定訂單的狀態欄位內容更新
-        console.log("AJAX -> DONE");
-        // console.log("---------------------------");
-        // console.log("updatedOrderDataMap 如下:");
-        // console.log(updatedOrderDataMap);
-        // console.log("updatedOrderDataMap.orderId 如下:");
-        // console.log(updatedOrderDataMap.orderId);
-        // console.log("updatedOrderDataMap.status 如下:");
-        // console.log(updatedOrderDataMap.status);
-        // console.log("type of updatedOrderDataMap.status ==> " + typeof(updatedOrderDataMap.status));
-        // console.log("---------------------------");
-
-        refreshClickedOrderItem(updatedOrderDataMap.orderId, updatedOrderDataMap.status);
-    }).fail(function() {
-        ////////// 可以在畫面塞入錯誤訊息提示，可以請使用者重新整理再操作，或是其他補救措施
-        // TODO
-        console.log("AJAX -> FAIL");
-    });
-}
-
 // 更新指定訂單的畫面
-function refreshClickedOrderItem(itemId, newStatus) {
+function refreshClickedOrderItem(itemId, newStatusNum) {
     console.log("refreshClickedOrderItem()");
 
+    // TODO
     // 更新該訂單的狀態文字
-    var $statusItem = $(`.order-id[value=${itemId}]`).closest(".operatebtn").siblings('.order-status')
-    $statusItem.text(newStatus);
+    var statusItem = $(`.order-id[value=${itemId}]`).closest(".operatebtn").siblings('.order-status').get(0);
+    $(statusItem).text(newStatusNum);
+
+
+    // 更新該訂單的狀態內容文字
+    var newStatusWord = changeStatusNumToWord(newStatusNum);
+    var $hiddenIdInput = $(`.order-id[value=${itemId}]`);
+    var $shipoutBtn = $hiddenIdInput.siblings('.status-shipout-btn');
+    var $statusItem = $hiddenIdInput.closest(".operatebtn").siblings('.order-status')
+    $statusItem.text(newStatusWord);
+
     // 更新該訂單操作按鈕的顯示
     var statusItem = $statusItem.get(0);
     refreshOperatorBtn(statusItem);
+}
+
+
+// 替換Thymeleaf帶入頁面的狀態數字，轉為邏輯意義中文文字
+function replaceStatusNumToWord(statusItem) {
+    console.log("replaceStatusNumToWord()");
+
+    // var statusNum = parseInt($(statusItem).text());
+    $(statusItem).text(changeStatusNumToWord($(statusItem).text()));
+}
+
+// 將訂單狀態數字轉為有邏輯意義的中文字，並傳回文字字串
+function changeStatusNumToWord(statusNum) {
+    console.log("changeStatusNumToWord()");
+    switch (statusNum) {
+        case 0:
+            // 待付款
+            return "待付款";
+        case 1:
+            // 待出貨
+            return "待出貨";
+        case 2:
+            // 待收貨
+            return "待收貨";
+        case 3:
+            // 完成
+            return "完成";
+        case 4:
+            // 取消
+            return "取消";
+        default:
+            // console.log("int -> 中文");
+    }
+}
+
+function changeStatusWordToNum(statusWord) {
+    // console.log("changeStatusWordToNum()");
+    switch (statusWord) {
+        case "待付款":
+            // 待付款
+            return 0;
+        case "待出貨":
+            // 待出貨
+            return 1;
+        case "待收貨":
+            // 待收貨
+            return 2;
+        case "完成":
+            // 完成
+            return 3;
+        case "取消":
+            // 取消
+            return 4;
+        default:
+            // console.log("int <- 中文");
+    }
 }
