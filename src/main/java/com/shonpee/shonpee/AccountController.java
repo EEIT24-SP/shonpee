0c53c599216ab7b62ab294a40ebe918f8a7fa27f
akikuma2762 <akikuma2762@gmail.com>
2021-07-02 02:26:03 +0800

    修正規格列印錯誤

--- src/main/java/com/shonpee/shonpee/AccountController.java
+++ src/main/java/com/shonpee/shonpee/AccountController.java
@@ -37,7 +37,7 @@ public class AccountController {
 	@RequestMapping(value = ("/main-page/acc"))
 	public String profile1(HttpSession session, Model model, MemberBean MB) {
 		String UserName = String.valueOf(session.getAttribute("UserName"));
-        if (UserName.isEmpty()) {
+        if (UserName.length()<=0) {
 			return "redirect:/login-page";
 		} else {
 			List<MemberBean> list = MR.findAll();
